6fa02c388d92f8384b529d5e337be03327663b89
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-08-02 17:51:59 +0700

    5.4.4. Task: Add a timer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -17,6 +18,12 @@ class GameViewModel : ViewModel() {
         get() = _score
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+    private val timer: CountDownTimer
+
 
 
     /**
@@ -55,20 +62,31 @@ class GameViewModel : ViewModel() {
         _word.value = ""
         _score.value = 0
         Log.i("GameViewModel", "GameViewModel created!")
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+
+            }
+        }
+        timer.start()
     }
     /**
      * Moves to the next word in the list
      */
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            resetList()
+        } else {
+            // Remove a word from the list
             _word.value = wordList.removeAt(0)
-            if (wordList.isEmpty()) {
-                onGameFinish()
-            } else {
-                //Select and remove a _word from the list
-                _word.value = wordList.removeAt(0)
-            }
+
         }
     }
     /** Methods for buttons presses **/
@@ -85,8 +103,10 @@ class GameViewModel : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        // Cancel the timer
+        timer.cancel()
     }
+
     private val _eventGameFinish = MutableLiveData<Boolean>()
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
@@ -97,5 +117,18 @@ class GameViewModel : ViewModel() {
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
     }
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
 
 }
