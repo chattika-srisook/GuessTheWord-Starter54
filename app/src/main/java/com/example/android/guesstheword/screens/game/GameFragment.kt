82c7358770ff9e366770cd366233e632aaa3cbed
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-08-02 17:18:39 +0700

    5.3Step 1: Add word LiveData to the game_fragment.xml file

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -56,9 +56,7 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         /** Setting up LiveData observation relationship **/
-        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
+        
 
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
@@ -70,6 +68,7 @@ class GameFragment : Fragment() {
             if (hasFinished) gameFinished()
         })
         binding.gameViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
     }
